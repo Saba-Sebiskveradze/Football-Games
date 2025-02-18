e7373bf1a4b23502af77ea7353fbab3ea714aea2
Saba-Sebiskveradze <ssebiskveradzee@gmail.com>
2025-02-18 17:49:37 +0400

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import ballLogo from '../../public/ballLogo.png'
 const Header: FC = () => {
   return (
     <header className="bg-#fff shadow-[0_1px_6px_0_rgba(0,0,0,0.07)]">
-      <div className="container mx-auto px-4 h-40 flex items-center justify-between ">
-        <Link to="/" className="group">
+      <div className="container mx-auto px-4 h-40 flex items-center justify-between">
+        <Link to="/" className='flex items-center justify-between '>
+          <img className='w-[80px] h-[80px]' src={ballLogo}></img>
           <h1 className="text-4xl md:text-6xl text-DavyGrey font-kavoon
                       transition-all duration-300 ease-in-out
                       group-hover:text-blue-400">
